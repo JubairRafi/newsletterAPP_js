51126b46a741c00bd19ce1b0edc7a70d0d6b0b8a
jobairRafi96 <jubairrafi96960@gmail.com>
2020-08-04 05:48:12 -0700

    server setting and post request

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public")); //for uploading static files
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/signup.html");//root path
+});
+
+app.post("/",(req,res)=>{
+  var firstName = req.body.fname;
+  var lastName = req.body.lname;
+  var email = req.body.email;
+});
+
 app.listen(3000,()=>{
   console.log("server started");
 });
